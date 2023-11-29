61a01d9b9e00367e1ac29fc629b93eb9ae8c3282
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-30 00:17:49 +0530

    updated cors origins

--- app/controllers/IndexController.java
+++ app/controllers/IndexController.java
@@ -5,6 +5,8 @@ import play.mvc.Result;
 import play.mvc.Http;
 import play.mvc.BodyParser;
 import java.util.HashMap;
+import java.util.Map;
+
 import play.libs.Json;
 
 public class IndexController extends Controller
@@ -15,25 +17,37 @@ public class IndexController extends Controller
 		public String b;
 	}
 
-	public Result get(String input)
+	public Result get(String input, Http.Request request)
 	{
-		return ok(Json.toJson(new HashMap<String, String>(){{put("input", input);}}));
+		return ok(Json.toJson(Map.of(
+			"input", input,
+			"headers", Json.toJson(request.getHeaders().asMap())
+		)));
 	}
 
 	@BodyParser.Of(BodyParser.Json.class)
 	public Result post(Http.Request request)
 	{
-		return ok(Json.toJson(new HashMap<String, InputClass>(){{put("input", Json.fromJson(request.body().asJson(), InputClass.class));}}));
+		return ok(Json.toJson(Map.of(
+			"input", Json.fromJson(request.body().asJson(), InputClass.class),
+			"headers", Json.toJson(request.getHeaders().asMap())
+		)));
 	}
 
 	@BodyParser.Of(BodyParser.Json.class)
 	public Result put(Http.Request request)
 	{
-		return ok(Json.toJson(new HashMap<String, InputClass>(){{put("input", Json.fromJson(request.body().asJson(), InputClass.class));}}));
+		return ok(Json.toJson(Map.of(
+				"input", Json.fromJson(request.body().asJson(), InputClass.class),
+				"headers", Json.toJson(request.getHeaders().asMap())
+		)));
 	}
 
-	public Result delete(String input)
+	public Result delete(String input, Http.Request request)
 	{
-		return ok(Json.toJson(new HashMap<String, String>(){{put("input", input);}}));
+		return ok(Json.toJson(Map.of(
+				"input", input,
+				"headers", Json.toJson(request.getHeaders().asMap())
+		)));
 	}
 }
